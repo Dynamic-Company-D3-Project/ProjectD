fcce73bf352686cb7570959e1eb3d87eb3da12c1
d3_83884_Abhilash <jabhilash0311@gmail.com>
2024-06-23 11:40:54 +0530

    modified app.jsx

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NavbarVertical from './components/NavbarVertical'
-import { Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
 <div>
-	<Routes>
-		<Route path="/" element={<NavbarVertical/>}/>
-	</Routes>
-	
+	<h1>Hi This is frontend react application </h1>  
+	<NavbarVertical/>
 </div>
 );
 }
