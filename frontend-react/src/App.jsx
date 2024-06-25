ac4dab52e5c76fa21413ff1dee7dd9d14ff38c23
d3_83884_Abhilash <jabhilash0311@gmail.com>
2024-06-25 20:40:25 +0530

    Added tested routes

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,6 +2,11 @@
 import Home from "./pages/Home";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Addresses from "./pages/Addresses";
+import CurrentBookings from "./pages/CurrentBookings";
+import UserDetails from "./pages/UserDetails";
+import PastBookings from "./pages/PastBookings";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
         <Route path={"/"} element={<Home />} />
         <Route path={"/services"} element={<Home />} />
         {/* <Route path={"/"+config.domain+"/signup/password"}  */}
+        {/* <Route path={"/Addresses"} element={<Addresses/>}/>
+        <Route path='CurrentBookings' element={<CurrentBookings />} />
+        <Route path='UserDetails' element={<UserDetails />} />
+        <Route path='PastBookings' element={<PastBookings />} />
+        <Route path='Cart' element={<Cart/>} /> */}
       </Routes>
     </div>
   );
