6236dad93e23f8dae25443ea3fd53337ba672b2d
dwivedialind <dwivedialind@gmail.com>
2024-06-26 10:24:48 +0530

    Nav Bar for admin

---
+++ frontend-react/src/Admin/components/AdminNavBar.jsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+
+function AdminNavBar() {
+  return (
+    <div className="navbar bg-base-100" style={{ backgroundColor: "#FFBF78" }}>
+      <div className="flex-1">
+        <Link className="btn btn-ghost text-xl">QuikEasy</Link>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="currentColor"
+          class="bi bi-person-workspace"
+          viewBox="0 0 16 16"
+        >
+          <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5" />
+          <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.4 5.4 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2z" />
+        </svg>
+      </div>
+      <div className="flex-none gap-2 not-italic hover:italic text-2xl">
+        <div className="text-blue-600">
+          <strong>Admin Name</strong>
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </Link>
+            </li>
+            <li>
+              <Link>Settings</Link>
+            </li>
+            <li>
+              <Link>Logout</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminNavBar;
