1aab978a69dc2c2bafdab04a6a66f379dc5de904
dwivedialind <dwivedialind@gmail.com>
2024-06-26 11:43:23 +0530

    modified links for Profile

--- frontend-react/src/Admin/components/AdminNavBar.jsx
+++ frontend-react/src/Admin/components/AdminNavBar.jsx
@@ -39,16 +39,12 @@ function AdminNavBar() {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <Link className="justify-between">
-                Profile
-                <span className="badge">New</span>
+              <Link to={"/admin/dashboard"} className="justify-between">
+                Dashboard
               </Link>
             </li>
             <li>
-              <Link>Settings</Link>
-            </li>
-            <li>
-              <Link>Logout</Link>
+              <Link to={"/admin/login"}>Logout</Link>
             </li>
           </ul>
         </div>
