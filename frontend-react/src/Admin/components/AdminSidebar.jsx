7fe087625dd3d4fc178038f4e48104c84e0bacdd
dwivedialind <dwivedialind@gmail.com>
2024-06-26 10:25:29 +0530

    Admin sideBar

---
+++ frontend-react/src/Admin/components/AdminSidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+
+const AdminSidebar = () => {
+  return (
+    <div className="Nav flex-column p-3 vh-100" style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+    <a href="/home" className="nav-link">Dashboard</a>
+    <a href="/orders" className="nav-link">Orders</a>
+    <a href="/providers" className="nav-link">Providers</a>
+    <a href="/settings" className="nav-link">Settings</a>
+  </div>
+  
+
+  );
+};
+
+export default AdminSidebar;
