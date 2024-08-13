9ee5585af252eccb95c25b859eb1ab934c272bb1
Abhilash-D-J <jabhilash0311@gmail.com>
2024-08-13 23:39:59 +0530

    changes to frontend

--- frontend-react/src/cards/HomeCard.jsx
+++ frontend-react/src/cards/HomeCard.jsx
@@ -23,7 +23,7 @@ export function HomeCategoryCard({ category_name, description,id }) {
 export function HomeSubCategoryCard({ category, subCategoryData,id }) {
   return (
     <Row>
-      {subCategoryData.map((sub) => {
+      {subCategoryData.sice(0,3).map((sub) => {
         return (
           <Col style={{ paddingLeft: "0px" }} key={sub.id}>
             {/* <a href={"/services/" + category + "/" + sub.categoryName}> */}
